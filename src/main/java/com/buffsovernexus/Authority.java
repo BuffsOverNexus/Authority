deedb29cc80125e8dd882f9a68cda78ca750e8eb
BuffsOverNexus <buffsovernexus2@gmail.com>
2022-09-04 16:57:43 -0400

    Initial commit

---
+++ src/main/java/com/buffsovernexus/Authority.java
@@ -0,0 +1,14 @@
+package com.buffsovernexus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Authority extends JavaPlugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+}
